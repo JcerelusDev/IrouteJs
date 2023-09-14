698a03dfe1a8083b38458b11ab1ec7785c521f82
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2023-09-13 22:41:41 -0400

    Update contact.js
    This is a functional based component

--- Example/public/contact.js
+++ Example/public/contact.js
@@ -1,3 +1,20 @@
-function Contact(){
-return  "This is Contact Us Page"
+const  Contact = () => {
+return  (
+`
+<center>
+<h3>Contact Page Component</h3>
+<p>This is a functional based page rendering
+
+<form>
+<label for="Name ">Your Name</label>
+<input type="text" />
+<br/><br/>
+<label for="E-mail ">Your Email</label>
+<input type="mail" />
+<br/><br/>
+<input type="submit" value="Submit" />
+</form>
+</center>
+`
+)
 }
