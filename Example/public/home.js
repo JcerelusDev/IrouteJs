345b5960369b1eaaa587444dfbc6abcf3e6a71b8
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2023-09-13 22:40:53 -0400

    Update home.js
    This is a functional based component

--- Example/public/home.js
+++ Example/public/home.js
@@ -1,4 +1,14 @@
+//functional base rendering
 var Home = function(){
-return  "This is Home Page"
+ return(
+ `
+<h3>Home page component</h3>
+<p>This is a functional based page rendering
 
+</p>
+ 
+ 
+ `
+ 
+ )
 }
