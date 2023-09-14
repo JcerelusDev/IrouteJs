7eb96d80e32e278a4aa0da47d5b8198f3b227b59
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2023-09-13 22:39:57 -0400

    Update about.js
    This is a web component based version

--- Example/public/about.js
+++ Example/public/about.js
@@ -1,4 +1,25 @@
-var About =()=>{
- return  "This is About Page"
- 
+
+
+const template = document.createElement("template")
+template.innerHTML =`
+<h3>About page component</h3>
+<p>This is a web component based page rendering
+
+</p>
+`
+
+class AboutPage extends HTMLElement{
+constructor(){
+super()
+
+var shadow = this.attachShadow({mode:"open"})
+shadow.appendChild(template.content.cloneNode(true))
+
+
 }
+
+
+
+}
+
+customElements.define("about-page",AboutPage)
